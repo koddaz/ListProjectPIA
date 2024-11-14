5e1ec0fb7b4cb8826380e4b94ba3e580f1e5f32e
koddaz <Albert.niklasson@gmail.com>
2024-11-14 10:54:41 +0100

    Initial Commit

---
+++ ListProjectPIA/ListProjectPIAApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListProjectPIAApp.swift
+//  ListProjectPIA
+//
+//  Created by Albert Niklasson on 2024-11-14.
+//
+
+import SwiftUI
+
+@main
+struct ListProjectPIAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
